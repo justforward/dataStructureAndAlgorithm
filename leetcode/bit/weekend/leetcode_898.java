27dfce76b1b311020c378ed0a59b77a7cfa5640e
tandi_wang <twang@yuansuan.com>
2022-09-28 10:27:22 +0800

    bit

--- leetcode/bit/weekend/leetcode_898.java
+++ leetcode/bit/weekend/leetcode_898.java
@@ -6,6 +6,9 @@ import java.util.Set;
 /**
  * @author wangtan
  * @date 2022/9/27 10:52 下午
+ * 思路：
+ * 使用一个集合cur存储以j为结尾的result值
+ * 这个result的值一直是递增的。| 的结果是单调递增的
  */
 public class leetcode_898 {
     public static void main(String[] args) {
@@ -20,15 +23,37 @@ public class leetcode_898 {
         Set<Integer> cur = new HashSet();
         cur.add(0);
         for (int x : arr) {
+            // 针对每个数，先记录以j为结尾的result值，下一个值跟cur中的每个值进行异或得到当前子数组中的值。一直进行
             Set<Integer> cur2 = new HashSet();
-            for (int y : cur){
+            for (int y : cur) {
                 cur2.add(x | y);
             }
             cur2.add(x);
             cur = cur2;
             ans.addAll(cur);
         }
+        return ans.size();
+    }
+
+
+    public int subBit(int[] arr) {
+        Set<Integer> ans = new HashSet<>();
+        Set<Integer> cur = new HashSet<>();
+        // 0|A=A 确定一开始的值就是遍历第一个结果的本身
+        cur.add(0);
+        for (int x : arr) {
+            Set<Integer> cur2 = new HashSet<>();
+            for (int c : cur) {
+                int tmp = x | c;
+                cur2.add(tmp);
+            }
+            // 将本次遍历的值也要添加到cur2中?
+            cur2.add(x);
+            cur = cur2;
+            ans.addAll(cur);
+        }
 
         return ans.size();
+
     }
 }
