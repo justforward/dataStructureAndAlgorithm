d2c1743ef909e3c7686a57cec6e717fd3d410662
tandi_wang <twang@yuansuan.com>
2022-10-20 20:47:12 +0800

    add

---
+++ leetcode/interview/i_05_01.java
@@ -0,0 +1,13 @@
+package leetcode.interview;
+
+/*
+    得到十进制的二进制
+ */
+public class i_05_01 {
+    public int insertBits(int N, int M, int i, int j) {
+        //
+        String n = Integer.toBinaryString(N);
+        String m = Integer.toBinaryString(M);
+        // 根据
+    }
+}
