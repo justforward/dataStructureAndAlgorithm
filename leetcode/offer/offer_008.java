168ee639d7806f58421194735baf980d404f9375
tandi_wang <twang@yuansuan.com>
2022-11-10 20:40:38 +0800

    add

---
+++ leetcode/offer/offer_008.java
@@ -0,0 +1,20 @@
+package leetcode.offer;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class offer_008 {
+    public int minSubArrayLen(int target, int[] nums) {
+
+        int right = nums.length;
+        int left = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        int tmp = 0;
+        while (left < right) {
+
+            while () {
+
+            }
+        }
+    }
+}
