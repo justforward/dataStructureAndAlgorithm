d2e8e7d199b07da1b23d26b41bf1768b80418b22
tandi_wang <twang@yuansuan.com>
2022-11-14 19:30:36 +0800

    day

--- leetcode/day/StockSpanner.java
+++ leetcode/day/StockSpanner.java
@@ -31,6 +31,36 @@ public class StockSpanner {
 
     }
 
+    public String customSortString(String order, String s) {
+        // 统计s的词频  根据order进行判断
+        int[] value = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            value[index]++;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) {
+            int cur = order.charAt(i) - 'a';
+            while (value[cur] > 0) {
+                stringBuilder.append(order.charAt(i));
+                value[cur]--;
+            }
+        }
+
+        // 直接遍历剩下的单词
+        for (int i = 0; i < 26; i++) {
+            while (value[i] > 0) {
+                stringBuilder.append((char) (i + 'a'));
+                value[i]--;
+            }
+        }
+        return stringBuilder.toString();
+
+    }
+
     public static void main(String[] args) {
+        String order = "cba", s = "abcd";
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.customSortString(order, s));
     }
 }
