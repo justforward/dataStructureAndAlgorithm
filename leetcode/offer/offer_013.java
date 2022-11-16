df827f4569a5fc156ac5870e26191aee9db33743
tandi_wang <twang@yuansuan.com>
2022-11-16 15:12:35 +0800

    add

---
+++ leetcode/offer/offer_013.java
@@ -0,0 +1,24 @@
+package leetcode.offer;
+
+
+/*
+    子矩阵的求和：
+        而这四个大矩阵有一个共同的特点，就是左上角都是 (0, 0) 原点。
+        四个
+ */
+public class offer_013 {
+
+
+    // 二维矩阵求和
+    class NumMatrix {
+
+        // 矩阵的初始化
+        public NumMatrix(int[][] matrix) {
+
+        }
+
+        public int sumRegion(int row1, int col1, int row2, int col2) {
+            //
+        }
+    }
+}
---
+++ 模板/前缀和/array.java
@@ -0,0 +1,13 @@
+package 模板.前缀和;
+
+public class array {
+    public static void main(String[] args) {
+
+    }
+
+    // 一维数组的前缀和
+
+
+    // 二维数组的前缀和
+
+}
