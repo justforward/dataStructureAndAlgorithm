7bf1245eaa110d8062264d1953b6cffd7e237e81
tandi_wang <twang@yuansuan.com>
2022-11-19 09:54:55 +0800

    add

---
+++ leetcode/offer/leetcode_018.java
@@ -0,0 +1,21 @@
+package leetcode.offer;
+
+public class leetcode_018 {
+    public boolean isPalindrome(String s) {
+        // 双指针
+        int left = 0, right = 0;
+        while (left < right) {
+            if(s.charAt())
+
+        }
+
+        return false;
+    }
+
+    /*
+        得到不同的
+     */
+    public boolean eq(){
+
+    }
+}
