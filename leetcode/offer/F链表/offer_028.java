90fe8061fe59319cd5da0a10ff9239960a48381c
tandi_wang <twang@yuansuan.com>
2022-12-01 15:39:16 +0800

    add

--- leetcode/offer/F链表/offer_028.java
+++ leetcode/offer/F链表/offer_028.java
@@ -8,20 +8,29 @@ public class offer_028 {
         public Node child;
     }
 
-
     class Solution {
-
         // 展开node 记录next
         public Node flatten(Node head) {
-            Node dummy;
-            while (head != null) {
-                Node next = head.next;
-                Node child = head.child;
-                // 得到文件的子节点
-                // 得到
-                while (child != null) {
-
+            Node cur = head;
+            while (cur != null) {
+                // 立即处理head所在的child层
+                if (cur.child != null) {
+                    // 首先存储的是当前的next，在处理完child所在层之后，就去对这个进行处理
+                    Node next = cur.next;
+                    // 这个指向child
+                    cur.next = cur.child;
+                    cur.child.prev = cur;
+                    cur.child = null;
+                    Node last = cur.next;
+                    // 找到本层的最后一个节点
+                    while (last.next != null) last = last.next;
+                    if (next != null) {
+                        last.next = next;
+                        next.prev = last;
+                    }
                 }
+                cur = cur.next;
+
             }
             return head;
         }
---
+++ leetcode/offer/F链表/offer_029.java
@@ -0,0 +1,28 @@
+package leetcode.offer.F链表;
+
+/*
+    给定循环单调非递减列表中的一个点， 插入这个点之后，依旧是单调递增的
+ */
+public class offer_029 {
+
+    class Node {
+        public int val;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _next) {
+            val = _val;
+            next = _next;
+        }
+    };
+
+
+    public Node insert(Node head, int insertVal) {
+
+    }
+}
