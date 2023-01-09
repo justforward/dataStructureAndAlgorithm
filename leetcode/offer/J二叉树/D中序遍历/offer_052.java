204950a38639a8f5131147d9f8b34e2c41dba791
tandi_wang <twang@yuansuan.com>
2023-01-09 09:45:46 +0800

    dya

---
+++ leetcode/offer/J二叉树/D中序遍历/offer_052.java
@@ -0,0 +1,31 @@
+package leetcode.offer.J二叉树.D中序遍历;
+
+public class offer_052 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode increasingBST(TreeNode root) {
+        // 中序遍历
+
+    }
+
+    public TreeNode inOrder() {
+
+    }
+}
