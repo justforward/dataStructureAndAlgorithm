b1eef2d4f585931e58a0784efab07667bc265cfd
tandi_wang <twang@yuansuan.com>
2023-01-15 13:40:39 +0800

    add

---
+++ leetcode/day/leetcode_2287.java
@@ -0,0 +1,43 @@
+package leetcode.day;
+
+
+/*
+
+ */
+public class leetcode_2287 {
+    public static void main(String[] args) {
+        leetcode_2287 leetcode_2287 = new leetcode_2287();
+        String s = "abbaccaddaeea", target = "aa";
+        int i = leetcode_2287.rearrangeCharacters(s, target);
+        System.out.println(i);
+    }
+
+    public int rearrangeCharacters(String s, String target) {
+        int ans = Integer.MAX_VALUE;
+        // 统计target的词频
+        int[] targetNum = new int[26];
+        for (int i = 0; i < target.length(); i++) {
+            int index = target.charAt(i) - 'a';
+            targetNum[index]++;
+        }
+
+        int[] sNum = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+            sNum[index]++;
+        }
+
+        for (int m = 0; m < 26; m++) {
+            if (targetNum[m] != 0) {
+                if (sNum[m] >= targetNum[m]) {
+                    ans = Math.min(sNum[m] / targetNum[m], ans);
+                } else {
+                    ans = 0;
+                }
+            }
+        }
+        return ans;
+    }
+
+
+}
---
+++ leetcode/offer/J二叉树/D中序遍历/offer_056.java
@@ -0,0 +1,50 @@
+package leetcode.offer.J二叉树.D中序遍历;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+public class offer_056 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<Integer> ans = new LinkedList<>();
+
+    public boolean findTarget(TreeNode root, int k) {
+        dfs(root);
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (Integer i : ans) {
+            if (map.containsKey(k - i)) {
+                return true;
+            }
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+        return false;
+    }
+
+
+    public void dfs(TreeNode root) {
+        if (root == null) return;
+        dfs(root.left);
+        ans.add(root.val);
+        dfs(root.right);
+    }
+
+
+}
